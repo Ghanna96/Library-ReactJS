fa580c2f729246b8aa0a66c43933e3dc6cf445cf
Ghanna96 <akramghanna@outlook.com>
2020-03-22 14:50:28 +0100

    Updates

---
+++ precache-manifest.e9b8e6977e607d85a183fa6d6f4f919f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7843ada3342c1c69a9502266446fffb8",
+    "url": "/Library-ReactJS/index.html"
+  },
+  {
+    "revision": "d987b686d17ac2643ecf",
+    "url": "/Library-ReactJS/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "050d316f38ca399719ed",
+    "url": "/Library-ReactJS/static/css/main.1a4d6b33.chunk.css"
+  },
+  {
+    "revision": "d987b686d17ac2643ecf",
+    "url": "/Library-ReactJS/static/js/2.25f26853.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Library-ReactJS/static/js/2.25f26853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050d316f38ca399719ed",
+    "url": "/Library-ReactJS/static/js/main.2f7281f6.chunk.js"
+  },
+  {
+    "revision": "0f741cd3a0ddf420834e",
+    "url": "/Library-ReactJS/static/js/runtime-main.e377b132.js"
+  }
+]);
